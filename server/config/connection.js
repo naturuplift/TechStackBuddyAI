db1e96a154bf710de2ddb4d6f7c6ea0221b7781f
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-04-17 01:45:44 -0300

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,41 +1,42 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI || 'fallback-local-database-uri';
+// const MONGODB_URI = process.env.MONGODB_URI || 'fallback-local-database-uri';
+
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// }).then(() => console.log('MongoDB connected successfully.'))
+//   .catch(err => console.error('MongoDB connection error:', err));
+
+// mongoose.connection.on('error', err => {
+//   console.error('MongoDB error:', err);
+//   if (err.message && err.message.code === 'ETIMEDOUT') {
+//     console.log('Attempting to re-connect to MongoDB...');
+//     mongoose.connect(MONGODB_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     });
+//   } else {
+//     console.error('Critical MongoDB connection error:', err);
+//     process.exit(1);
+//   }
+// });
+
+// Mongo URI connection string 
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/StackBuddyAI_DB';
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected successfully.'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
-mongoose.connection.on('error', err => {
-  console.error('MongoDB error:', err);
-  if (err.message && err.message.code === 'ETIMEDOUT') {
-    console.log('Attempting to re-connect to MongoDB...');
-    mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-  } else {
-    console.error('Critical MongoDB connection error:', err);
-    process.exit(1);
-  }
 });
+ // Connect to Mongo
+    mongoose.connection.on('connected', () => {
+        console.log('Connected to MongoDB');
+    });
+    // Log Connection Error 
+    mongoose.connection.on('error', (err) => {
+        console.error(`Error connecting to MongoDB: ${err.message}`);
+    });
 
-// // Mongo URI connection string 
-// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/StackBuddyAI_DB';
-
-// mongoose.connect(MONGODB_URI, {
-
-// });
-//  // Connect to Mongo
-//     mongoose.connection.on('connected', () => {
-//         console.log('Connected to MongoDB');
-//     });
-//     // Log Connection Error 
-//     mongoose.connection.on('error', (err) => {
-//         console.error(`Error connecting to MongoDB: ${err.message}`);
-//     });
-
-//     // Export the connection 
-//     module.exports = mongoose.connection; 
+    // Export the connection 
+    module.exports = mongoose.connection; 
