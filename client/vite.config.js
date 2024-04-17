7543bb26a0518f723474477cb050e1e4f629ec17
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-04-17 00:22:19 -0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,24 +9,10 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({ 
-      open: true, 
-      gzipSize: true, 
-      brotliSize: true 
-    })
-  ],
+  plugins: [react()],
   build: {
     rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0]; // Split vendor code into separate chunks
-          }
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000, // Increase the chunk size warning limit if needed
+      external: ['react-quill/dist/quill.snow.css']
+    }
   }
 });
