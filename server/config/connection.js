ac483cc2018f9c3a745dd5110ed94b8e410a73a8
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-04-17 01:49:02 -0300

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -25,10 +25,7 @@ const mongoose = require('mongoose');
 // Mongo URI connection string 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/StackBuddyAI_DB';
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(MONGODB_URI);
  // Connect to Mongo
     mongoose.connection.on('connected', () => {
         console.log('Connected to MongoDB');
