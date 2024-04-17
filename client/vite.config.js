de0c49b03de856a6bb436a39afbf0755971771bf
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-04-16 23:43:20 -0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { visualizer } from 'rollup-plugin-visualizer';
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    visualizer({ open: true, gzipSize: true, brotliSize: true }) // Optional: Visualizes bundle size
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0]; // Split vendor code into separate chunks
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000, // Increase the chunk size warning limit if needed
+  }
+});
