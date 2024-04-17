7498de6d2a60cb4faaa2010db91e80e8d63190d3
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-04-17 01:19:45 -0300

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,19 +1,37 @@
 const mongoose = require('mongoose');
 
-// Mongo URI connection string 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/StackBuddyAI_DB';
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true, // Ensure SSL is enabled
+  sslValidate: true,
+}).then(() => console.log('MongoDB connected successfully.'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
-mongoose.connect(MONGODB_URI, {
-
+mongoose.connection.on('error', err => {
+  console.error('MongoDB error:', err);
+  // Exit if the database connection is critical
+  if (err.message.code === 'ETIMEDOUT') {
+    console.log('Attempting to re-connect to MongoDB...');
+    mongoose.connect(process.env.MONGODB_URI);
+  } else {
+    process.exit(1);
+  }
 });
- // Connect to Mongo
-    mongoose.connection.on('connected', () => {
-        console.log('Connected to MongoDB');
-    });
-    // Log Connection Error 
-    mongoose.connection.on('error', (err) => {
-        console.error(`Error connecting to MongoDB: ${err.message}`);
-    });
+// // Mongo URI connection string 
+// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/StackBuddyAI_DB';
 
-    // Export the connection 
-    module.exports = mongoose.connection; 
+// mongoose.connect(MONGODB_URI, {
+
+// });
+//  // Connect to Mongo
+//     mongoose.connection.on('connected', () => {
+//         console.log('Connected to MongoDB');
+//     });
+//     // Log Connection Error 
+//     mongoose.connection.on('error', (err) => {
+//         console.error(`Error connecting to MongoDB: ${err.message}`);
+//     });
+
+//     // Export the connection 
+//     module.exports = mongoose.connection; 
